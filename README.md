ec4bd3d2f082e58dcc32e5027f8145cace0d221d
Siska Umami <itssiskaa27@gmail.com>
2025-05-01 15:03:28 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# project_akhir1
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  //supabase setup
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: "https://iaraofinpyowiiiytsug.supabase.co",
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhcmFvZmlucHlvd2lpaXl0c3VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MjE2MDEsImV4cCI6MjA2MTQ5NzYwMX0.qCIhdoKF8SRUIJlZ26op_IxpTkHMnvEXwGY2PMU_-h0",
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    );
+  }
+}
